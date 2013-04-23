de8e0514c437c688626e70fae80f3a18a38e015a
Óscar Rodríguez Bouza <mazonovo@gmail.com>
2013-04-23 23:11:49 +0200

    Creado el EM con el esquema de la BD
    Falta ajustar los espacios de nombres. Aun no esta generado el código ni
    la LocalDB aunque aparezcan las clases.

---
+++ EntrecineWebApp/Models/EntityModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\Desarrollo\Documents\GitHub\Entrecine8\EntrecineWebApp\Models\EntityModel.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
