4f09a18ee2e361b3ef50d552f85e0508b2d60f0c
Óscar Rodríguez Bouza <mazonovo@gmail.com>
2013-04-28 13:38:48 +0200

    Añadidas anotaciones EF via clases parciales
    Se cambia el path de registro a nuestro sistema de usuarios. Borro la
    prueba que se había hecho con movies.

---
+++ EntrecineWebApp/Models/UsuarioAnnotations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EntrecineWebApp.Models
+{
+    [MetadataType(typeof(UsuarioAnnotations))]
+    public partial class Usuario
+    {
+    }
+
+    public class UsuarioAnnotations
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellidos { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre de usuario")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DefaultValue(false)]
+        public bool Administrador { get; set; }
+    
+    }
+}
