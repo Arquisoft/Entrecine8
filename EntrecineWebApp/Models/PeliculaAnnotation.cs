dfdc04ddfb6d1858acab65012ed2f721e7cd0084
Abdón Rodríguez Pérez <astur83@gmail.com>
2013-04-28 17:47:44 +0200

    Creacion peliculaAnnotation.cs y prueba con gitExtensions

---
+++ EntrecineWebApp/Models/PeliculaAnnotation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EntrecineWebApp.Models
+{
+    [MetadataType(typeof(PeliculaAnnotation))]
+    public partial class Pelicula
+    {
+    }
+
+    public class PeliculaAnnotation
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Título")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Precio base")]
+        public double Precio { get; set; }
+        
+        [Required]
+        [DataType(DataType.Duration)]
+        [Display(Name = "Duración (minutos)")]
+        public int Duracion { get; set; }
+    }
+
+}
