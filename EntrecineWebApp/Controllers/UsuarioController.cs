12f79d3e27600579fa75c184baeb4258f9c935a8
Óscar Rodríguez Bouza <mazonovo@gmail.com>
2013-04-28 12:24:27 +0200

    Layout home + controlador usuario
    Añado el nuevo layout para la pagina principal de nuestro sitio y creo
    el controlador para el elemento usuario de nuestro modelo.

---
+++ EntrecineWebApp/Controllers/UsuarioController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntrecineWebApp.Models;
+
+namespace EntrecineWebApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private EntrecineModelContainer db = new EntrecineModelContainer();
+
+        //
+        // GET: /Usuario/
+
+        public ActionResult Index()
+        {
+            return View(db.UsuarioConjunto.ToList());
+        }
+
+        //
+        // GET: /Usuario/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Usuario usuario = db.UsuarioConjunto.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Usuario/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UsuarioConjunto.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Usuario usuario = db.UsuarioConjunto.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /Usuario/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        //
+        // GET: /Usuario/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Usuario usuario = db.UsuarioConjunto.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        //
+        // POST: /Usuario/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuario usuario = db.UsuarioConjunto.Find(id);
+            db.UsuarioConjunto.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
