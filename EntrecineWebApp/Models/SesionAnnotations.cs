9d889a3edd3b5ab35702032e9109f4d60a699704
Óscar Rodríguez Bouza <mazonovo@gmail.com>
2013-05-02 16:20:45 +0200

    Mejorada la vista de detalles de pelicula
    + Campo PrecioCalculado en SesionAnnotations

---
+++ EntrecineWebApp/Models/SesionAnnotations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace EntrecineWebApp.Models
+{
+    [MetadataType(typeof(SesionAnnotations))]
+    public partial class Sesion
+    {
+        public double PrecioCalculado
+        {
+            get
+            {
+                return Pelicula.Precio - (Pelicula.Precio * Descuento.Porcentaje);
+            }
+        }
+    }
+
+    public class SesionAnnotations
+    {
+
+    }
+}
