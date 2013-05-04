7e331e27bd4ffa75dfb1fe83ea943ae5ae679b05
Óscar Rodríguez Prieto <chunguito.gullit@gmail.com>
2013-05-04 12:17:01 +0200

    Recomendaciones Personales
    Se añaden al apartado Recomendaciones, las películas que no Vio el
    cliente y si vio el cliente que tiene más películas vistas en común con
    él. Se ha modificado RecomendacionesModel y el Index de Recomendaciones.

--- EntrecineWebApp/Models/RecomendacionesModel.cs
+++ EntrecineWebApp/Models/RecomendacionesModel.cs
@@ -20,6 +20,17 @@ namespace EntrecineWebApp.Models
         [Display(Name = "MasVistas")]
         public IList<Pelicula> PelisMasVistas { get { return new EntrecineModelContainer().PeliculaConjunto.OrderByDescending(VecesVista).Take(3).ToList(); } }
 
+        public IEnumerable<Pelicula> Recomendaciones(String nombre)
+        {
+            EntrecineModelContainer db = new EntrecineModelContainer();
+            Usuario cliente = db.UsuarioConjunto.First(u => u.Nombre == nombre);
+            Func<Usuario, IEnumerable<Pelicula>> pelisQueVio = usuario => db.ReservaConjunto.Where(r => r.Usuario.Id == usuario.Id).Select(r => r.Sesion.Pelicula);
+            Func<Usuario, int> nPelisEnComun = usuario =>   pelisQueVio(usuario).Intersect(pelisQueVio(cliente)).Count();
+            IEnumerable<Pelicula> recomendaciones = pelisQueVio(db.UsuarioConjunto.Where(u=>u.Id!=cliente.Id).OrderByDescending(nPelisEnComun).First()).Where(p=>!pelisQueVio(cliente).Contains(p));
+            return recomendaciones;
+        }
+
+
 
     }
 }
